ee345ad7ff7da01782d543d3fae6b737a92b851a
Richard Lemon <richard@codelemon.com>
2018-02-11 13:11:52 +1100

    added the IO handling

--- main.py
+++ main.py
@@ -7,13 +7,34 @@ PIN_LED    = const(13)
 led    = None
 relay  = None
 button = None
+def check_inputs():
+    '''Check the digital IO and set the relay accordingly'''
+    change_relay = False
+    while (button.value() == 0):
+        # button pressed
+        change_relay = True
+    if (change_relay):
+        if(relay.value() == 1):
+            relay.off()
+        else:
+            relay.on()
+    if (relay.value() == 0):
+        led.off()
+    else:
+        led.on()
+
+def do_webstuff():
+    '''Handle any webserver requests'''
+
 
 def teardown():
+    '''Should never be run'''
     # oops alert
     # disconnect
     # relay off
 
 def setup():
+    '''Setup the system (run once at start)'''
     # setup IO
     button = machine.Pin(PIN_BUTTON, machine.Pin.IN)
     relay  = machine.Pin(PIN_RELAY,  machine.Pin.OUT)
@@ -22,6 +43,7 @@ def setup():
 
 
 def main_loop():
+    '''Main runloop should never exit'''
     while 1:
         # do the things
         check_inputs()
